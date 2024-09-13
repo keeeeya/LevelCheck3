03ceade0b1bee3ab43108cac3b8053fad511c144
keikei <lvc_k18@icloud.com>
2024-09-14 06:03:56 +0900

    「初級理解度チェック　問題３」の修正をコミット

--- src/main/java/org/example/NumberChecker.java
+++ src/main/java/org/example/NumberChecker.java
@@ -2,10 +2,9 @@ package org.example;
 
 public class NumberChecker {
 
-  Telephone telephone = new Telephone();
 
   public void checkNumber(String number) {
-    telephone.numbers.add(new Telephone(number));
+
     if (number.matches("\\d{3}-\\d{4}-\\d{4}")) {
       System.out.println(number + "　は有効な携帯電話番号です。");
     } else {
